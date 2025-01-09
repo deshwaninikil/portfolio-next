57c715000f154066c30feb17288ae62a653bd860
deshwaninikil <79966144+deshwaninikil@users.noreply.github.com>
2025-01-09 14:10:12 +0530

    Update code

--- components/WebCloneDesign.tsx
+++ components/WebCloneDesign.tsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import ProjectCard from './UI/ProjectCard'
-import { ClonedData } from './Data/Cloneddata'
+import React from "react";
+import ProjectCard from "./UI/ProjectCard";
+import { projectData } from "./Data/ProjectData";
 
 export default function WebCloneDesign() {
   return (
     <div className="flex flex-wrap justify-center">
-    {ClonedData.map((project, index) => (
-      <ProjectCard
-        key={index}
-        img={project.img} 
-        title={project.title}
-        githubLink={project.githubLink}
-        deployedLink={project.deployedLink}
-        skil= {project.skill}
-        description={project.description}
-
-      />
-    ))}
-  </div>
-  )
+      {projectData.map((project, index) => (
+        <ProjectCard
+          key={index}
+          img={project.img}
+          title={project.title}
+          githubLink={project.githubLink}
+          deployedLink={project.deployedLink}
+          skil={project.skill}
+          description={project.description}
+        />
+      ))}
+    </div>
+  );
 }
